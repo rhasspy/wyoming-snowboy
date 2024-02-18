2add766e63b97d31a125c596c9ed8b6d21b34a4f
Michael Hansen <mike@rhasspy.org>
2024-02-18 16:05:03 -0600

    No Python 3.12 again

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
     ],
     keywords="rhasspy wyoming snowboy wake word",
     cmdclass={"build": SnowboyBuild},
